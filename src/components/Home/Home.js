d1a062a5105045023db011e5392991f5d2ab1720
riskymind <oparaiskelechi@gmail.com>
2020-03-17 23:17:42 +0100

    fetch the api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../../config";
 import HeroImage from "../elements/HeroImage/HeroImage";
 import SearchBar from "../elements/SearchBar/SearchBar";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
@@ -8,7 +15,50 @@ import Spinner from "../elements/Spinner/Spinner";
 import "./Home.css";
 
 class Home extends Component {
-  state = {};
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-us&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  loadMoreItems = () => {
+    let endpoint = "";
+    this.state({ loading: true });
+
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-us$page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `{${API_URL}}search/movie?api_key=${API_KEY}$language=en-us$query${
+        this.state.searchTerm
+      }$page=${this.state.currentPage + 1}`;
+    }
+
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.totalPages
+        });
+      });
+  };
 
   render() {
     return (
